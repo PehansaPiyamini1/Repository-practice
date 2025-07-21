7b2587cadc834e4c4ac43f6ce5240585c803aecb
PehansaPiyamini1 <as20240469@sci.sjp.ac.lk>
2025-07-21 18:28:11 +0530

    Create a new file (repository1.c)

---
+++ repository1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  printf("My name is P.P.G.Pehansa Piyamini......\n");
+  printf("My age is 22......\n");
+  printf("I like stay myself freely.........\n");
+
+return 0;
+}
